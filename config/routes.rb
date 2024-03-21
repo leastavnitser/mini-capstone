a6ebfa8182698feed428ea6ff002767ca500085d
leastavnit <lea.stavnitser@gmail.com>
2024-03-21 12:29:05 -0500

    adds user test and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+  post "/users" => "users#create"
   resources :suppliers
   resources :images
   # Defines the root path route ("/")
