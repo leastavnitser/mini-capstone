2a486d78db38fd23a1026886dfd3eaff9a561276
leastavnit <lea.stavnitser@gmail.com>
2024-03-22 13:53:26 -0500

    adds crud for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,41 @@
 class OrdersController < ApplicationController
+  def show
+    @order = Order.find_by(id: params[:id])
+    render :show
+  end
+
+  def index
+    @orders = Order.all
+    render :index
+  end
+
+  def create(
+    user_id: params["user_id"],
+    product_id: params["product_id"],
+    quantity: params["quantity"],
+    subtotal: params["subtotal"],
+    tax: params["tax"],
+    total: params["total"]
+  )
+    render :show
+  end
+
+  def update
+    @orders = Order.find_by(id: params[:id])
+    @product.update(
+      user_id: params["user_id"] || @order.name,
+      product_id: params["product_id"] || @order.product_id,
+      quantity: params["quantity"] || @order.quantity,
+      subtotal: params["subtotal"] || @order.subtotal,
+      tax: params["tax"] || @order.tax,
+      total: params["total"] || @order.total,
+    )
+    render :show
+  end
+
+  def destroy
+    @order = Order.find_by(id: params[:id])
+    @order.destroy
+    render json: { message: "Data successfully destroyed!" }
+  end
 end
--- app/views/orders/_order.json.jbuilder
+++ app/views/orders/_order.json.jbuilder
@@ -1,2 +1,9 @@
-json.extract! order, :id, :created_at, :updated_at
-json.url order_url(order, format: :json)
+json.id order.id
+json.user_id order.user_id
+json.product_id order.product_id
+json.quantity order.quantity
+json.subtotal order.subtotal
+json.tax order.tax
+json.total order.total
+json.created_at order.created_at
+json.updated_at order.updated_at
