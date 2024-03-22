1312e687be0ccf6b35a5e066d91e8db5aeed1a0a
leastavnit <lea.stavnitser@gmail.com>
2024-03-22 15:19:09 -0500

    corrections to orders show and intex

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,24 @@
 class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
-    render :show
+    render json: @order.as_json
   end
 
   def index
     @orders = Order.all
-    render :index
+    render json: @orders.as_json
   end
 
-  def create(
-    user_id: params["user_id"],
-    product_id: params["product_id"],
-    quantity: params["quantity"],
-    subtotal: params["subtotal"],
-    tax: params["tax"],
-    total: params["total"]
-  )
+  def create
+    @order = Order.create(
+      user_id: current_user.id,
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: params["subtotal"],
+      tax: params["tax"],
+      total: params["total"],
+    )
+    if @order
     render :show
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  def subtotal
+    Product.find_by(id: params[:price]) * quantity
+  end
+
+  def
 end
